d8206509901e48323739cf51c6e615c52e8a4911
nathandaniels <nathandaniels@mac.com>
2020-05-31 13:42:56 +0930

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import serial
 from time import sleep
 ser = serial.Serial('/dev/ttyUSB1', timeout=1)
 ser.baudrate = 115200
-
-msg = 'test'
-ser.write(msg)
-sleep(0.5)
-ser.readline()
+while True:
+  msg = 'test'
+  ser.write(msg)
+  sleep(0.5)
+  #ser.readline()
