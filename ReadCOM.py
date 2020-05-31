027ed26bb02989d26ea50541a66a7a2482960e27
nathandaniels <nathandaniels@mac.com>
2020-05-31 14:45:41 +0930

    Update ReadCOM.py

--- ReadCOM.py
+++ ReadCOM.py
@@ -1,14 +1,12 @@
-import sys
 import serial
 
-ser1 = serial.Serial("/dev/ttyUSB0", "115200")
-ser2 = serial.Serial("/dev/ttyUSB1", "115200")
+ser1 = serial.Serial("/dev/ttyUSB0", "115200", timeout=1.0)
+ser2 = serial.Serial("/dev/ttyUSB1", "115200", timeout=1.0)
+
 while True: 
     output1 = ser1.readline()
     output2 = ser2.readline()
     ser1.write(output2)
     ser2.write(output1)
-    sys.stdout.write(output2)
-    sys.stdout.write(output1)
-    sys.stdout.flush()
-   
+    ser1.close()
+    ser2.close()
