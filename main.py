568ffd15f3418cf297fa2f7c1f790e72a42255d6
nathandaniels <nathandaniels@mac.com>
2020-05-31 14:36:11 +0930

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 from subprocess import Popen, PIPE
-p1 = Popen(['python', './ReadCOM.py', "/dev/ttyUSB0", "115200"], stdin=PIPE, stdout=PIPE, stderr=PIPE) # read COM1 permanently
-p2 = Popen(['python', './ReadCOM.py', "/dev/ttyUSB1", "115200"], stdin=PIPE, stdout=PIPE, stderr=PIPE) # read COM2 permanently
+p1 = Popen(['python', './ReadCOM.py'], stdin=PIPE, stdout=PIPE, stderr=PIPE) # read COM1 permanently
 
 finished = False
 while not finished:
     print "received from COM1: %s" % p1.stdout.readline() # print output from ReadCOM.py for COM1
-    print "received from COM2: %s" % p2.stdout.readline() # print output from ReadCOM.py for COM2
-    
-    ser1 = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.5)
-    ser1.write(p2.stdout.readline())
-    time.sleep(0.1)
-    ser1.close()
-    
-    ser2 = serial.Serial('/dev/ttyUSB1', 115200, timeout=0.5)
-    ser2.write(p1.stdout.readline())
-    time.sleep(0.1)
-    ser2.close()
