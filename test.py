16246217eed3a6d3d162cda6c8c7eaa491fa5b3b
nathandaniels <nathandaniels@mac.com>
2020-05-31 14:08:27 +0930

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import serial
-ser1 = serial.Serial('/dev/ttyUSB0', timeout=1)
+ser1 = serial.Serial('/dev/ttyUSB0')
 ser1.baudrate = 115200
-ser2 = serial.Serial('/dev/ttyUSB1', timeout=1)
+ser2 = serial.Serial('/dev/ttyUSB1')
 ser2.baudrate = 115200
-
 while True:
   output1 = ser1.readline()
   output2 = ser2.readline()
