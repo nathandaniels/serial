144371d760077b90a053098b3739c72b4d6fd299
nathandaniels <nathandaniels@mac.com>
2020-05-31 14:35:21 +0930

    Update ReadCOM.py

--- ReadCOM.py
+++ ReadCOM.py
@@ -1,12 +1,22 @@
 import sys
 import serial
 
-ser = serial.Serial(port=sys.argv[1],baudrate=int(sys.argv[2]))
+ser1 = serial.Serial("/dev/ttyUSB0", "115200")
 while True:  # The program never ends... will be killed when master is over.
     # sys.stdin.readline()
 
-    ser.write('serial command here\n') # send command to serial port
-    output = ser.readline() # read output
+    ser1.write(output2) # send command to serial port
+    output1 = ser.readline() # read output
 
-    sys.stdout.write(output) # write output to stdout
+    sys.stdout.write(output1) # write output to stdout
     sys.stdout.flush()
+    
+ser2 = serial.Serial("/dev/ttyUSB1", "115200")
+while True:  # The program never ends... will be killed when master is over.
+    # sys.stdin.readline()
+
+    ser2.write(output1) # send command to serial port
+    output2 = ser.readline() # read output
+
+    sys2.stdout.write(output2) # write output to stdout
+    sys2.stdout.flush()
