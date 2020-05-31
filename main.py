ccfef8bab0fa6545192a2e72040c95a181294fd4
nathandaniels <nathandaniels@mac.com>
2020-05-31 12:27:14 +0930

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ from subprocess import Popen, PIPE
 p1 = Popen(['python', './ReadCOM.py', "/dev/ttyUSB0", "115200"], stdin=PIPE, stdout=PIPE, stderr=PIPE) # read COM1 permanently
 p2 = Popen(['python', './ReadCOM.py', "/dev/ttyUSB1", "115200"], stdin=PIPE, stdout=PIPE, stderr=PIPE) # read COM2 permanently
 
-for i in range(10):
+While True:
     print "received from COM1: %s" % p1.stdout.readline() # print output from ReadCOM.py for COM1
     print "received from COM2: %s" % p2.stdout.readline() # print output from ReadCOM.py for COM2
