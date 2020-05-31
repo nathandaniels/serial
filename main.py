11457f61897e76b760b85431fa71ecc46209e049
nathandaniels <nathandaniels@mac.com>
2020-05-31 14:25:03 +0930

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ finished = False
 while not finished:
     print "received from COM1: %s" % p1.stdout.readline() # print output from ReadCOM.py for COM1
     print "received from COM2: %s" % p2.stdout.readline() # print output from ReadCOM.py for COM2
+    
+    ser1 = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.5)
+    ser1.write(p2.stdout.readline())
+    time.sleep(0.1)
+    ser1.close()
+    
+    see2 = serial.Serial('/dev/ttyUSB1', 115200, timeout=0.5)
+    ser2.write(p1.stdout.readline())
+    time.sleep(0.1)
+    ser2.close()
