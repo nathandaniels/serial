412a8365bace4e1d3d5d4070f271e270dee9ac83
nathandaniels <nathandaniels@mac.com>
2020-05-31 14:38:13 +0930

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from subprocess import Popen, PIPE
-p1 = Popen(['python', './ReadCOM.py'], stdin=PIPE, stdout=PIPE, stderr=PIPE) # read COM1 permanently
-
-finished = False
-while not finished:
-    print "received from COM1: %s" % p1.stdout.readline() # print output from ReadCOM.py for COM1
