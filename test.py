1e114c9e7b4177ea39a22439ddcd77d1292552df
nathandaniels <nathandaniels@mac.com>
2020-05-31 14:36:25 +0930

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-while True:
-  p1 = Popen(['python', './ReadCOM.py', "/dev/ttyUSB0", "115200"], stdin=PIPE, stdout=PIPE, stderr=PIPE) # read COM1 permanently
-  p2 = Popen(['python', './ReadCOM.py', "/dev/ttyUSB1", "115200"], stdin=PIPE, stdout=PIPE, stderr=PIPE) # read COM2 permanently
-  print "received from COM1: %s" % p1.stdout.readline() # print output from ReadCOM.py for COM1
-  print "received from COM2: %s" % p2.stdout.readline() #
-  
