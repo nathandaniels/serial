e157268d60d269c33f6e1a1a3ac90f56d4b2fd65
nathandaniels <nathandaniels@mac.com>
2020-05-31 15:49:55 +0930

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import serial
 
-serialport0 = serial.Serial("/dev/ttyUSB0", 115200, timeout=0.5)
-serialport1 = serial.Serial("/dev/ttyUSB1", 115200, timeout=0.5)
+serialportzero = serial.Serial("/dev/ttyUSB0", 115200, timeout=0.5)
+serialportone = serial.Serial("/dev/ttyUSB1", 115200, timeout=0.5)
 
 while True:    
-    command0 = serialport0.read()
-    print str(command0)
-    serialport1.write(command0.encode())
-    command1 = serialport1.read()
-    print (str(command1)
-    serialport0.write(command1.encode())
+    commandzero = serialportzero.read()
+    print str(commandzero)
+    serialportone.write(command0.encode())
+    commandone = serialportone.read()
+    print (str(commandone)
+    serialportzero.write(commandone.encode())
 
