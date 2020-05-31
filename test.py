1e4c2a104e4e7f453a9e690aa87cf295b0231c30
nathandaniels <nathandaniels@mac.com>
2020-05-31 15:51:16 +0930

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ serialportone = serial.Serial("/dev/ttyUSB1", 115200, timeout=0.5)
 while True:    
     commandzero = serialportzero.read()
     print str(commandzero)
-    serialportone.write(command0.encode())
+    serialportone.write(commandzero.encode())
     commandone = serialportone.read()
     print (str(commandone)
     serialportzero.write(commandone.encode())
